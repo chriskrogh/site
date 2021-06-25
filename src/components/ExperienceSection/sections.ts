822834cc6936e2bce5b20951f21271299f5d499f
Christopher Krogh <chris.krogh@outlook.com>
2021-06-25 12:49:05 -0400

    added descriptions
    updated description

--- src/components/ExperienceSection/sections.ts
+++ src/components/ExperienceSection/sections.ts
@@ -9,6 +9,9 @@ export const ExperienceSections: Record<EXPERIENCE_SECTION, ExperienceSection> =
       title: 'Dot',
       descriptions: [
         'Architected, designed, and implemented an ewallet platform for Trinidad and Tobago!',
+        'Engineered a GraphQL microservice backend using Apollo Federation that handles authentication and processes peer-to-peer and VISA transactions.',
+        'Configured container orchestration using Kubernetes to integrate with GitHub actions to manage the CICD pipeline.',
+        'Crafted a modern, mobile wallet user experience in Figma and implemented it with Ionic Framework and React.',
       ],
       link: 'https://dot.cash',
       tech: [
@@ -32,9 +35,9 @@ export const ExperienceSections: Record<EXPERIENCE_SECTION, ExperienceSection> =
       image: '/assets/faire_logo.png',
       title: 'Faire',
       descriptions: [
-        'Built complete features using React like carousels and banners for Faire Winter Market, a search experience for editing preorders, and prompts for fixing cart issues.',
+        'Built amazing experiences using React like carousels and banners for Faire Winter Market, a search component for editing preorders, and prompts for fixing cart issues.',
         'Restructured an internal NodeJS server into a monorepo using yarn workspaces to add a frontend for triggering actions manually.',
-        'Configured end to end tests to also run on mobile web using Cypress to increase mobile test coverage.',
+        'Configured end to end tests to also run on mobile web using Cypress to increase test coverage.',
       ],
       link: 'https://faire.com',
       tech: [TECH.TYPESCRIPT, TECH.REACT, TECH.NODE],
@@ -47,7 +50,10 @@ export const ExperienceSections: Record<EXPERIENCE_SECTION, ExperienceSection> =
     [EXPERIENCE_SECTION.SYS]: {
       image: '/assets/sys_logo.png',
       title: 'Sell Your Stuff',
-      descriptions: [''],
+      descriptions: [
+        'Implemented an order management experience using React and Apollo for sellers on the platform.',
+        'Introduced product and engineering design docs to streamline communication between team members.',
+      ],
       link: 'https://sellyourstuff.tt/',
       tech: [
         TECH.TYPESCRIPT,
@@ -65,7 +71,10 @@ export const ExperienceSections: Record<EXPERIENCE_SECTION, ExperienceSection> =
     [EXPERIENCE_SECTION.WEBVELOPER]: {
       image: '/assets/webveloper_logo.png',
       title: 'Webveloper',
-      descriptions: [''],
+      descriptions: [
+        'Implemented performant UI components using memoization to provide a snappy editor experience.',
+        'Reduced dev server build time from ~30s to ~10s by implementing hot module reloading with custom webpack config.',
+      ],
       link: 'https://webveloper.com/',
       tech: [TECH.TYPESCRIPT, TECH.REACT, TECH.NODE],
       position: 'Software Engineer Co-Op',
@@ -77,7 +86,11 @@ export const ExperienceSections: Record<EXPERIENCE_SECTION, ExperienceSection> =
     [EXPERIENCE_SECTION.INTUIT]: {
       image: '/assets/intuit_logo.png',
       title: 'Intuit',
-      descriptions: [''],
+      descriptions: [
+        'Implemented new pages in TurboTax using object-oriented Java with 100% diff coverage on unit tests.',
+        'Reduced TurboTax build time by 45% by parallelizing Typescript compilation with Gulp, saving teammates 40 hours/year.',
+        'Automated large manual tasks like regression test script conversion using Replay and VM deployment using PowerShell',
+      ],
       link: 'https://www.intuit.com/',
       tech: [TECH.JAVA, TECH.TYPESCRIPT, TECH.NODE],
       position: 'Software Engineer Co-Op',
@@ -89,7 +102,10 @@ export const ExperienceSections: Record<EXPERIENCE_SECTION, ExperienceSection> =
     [EXPERIENCE_SECTION.MANULIFE]: {
       image: '/assets/manulife_logo.png',
       title: 'Manulife Financial',
-      descriptions: [''],
+      descriptions: [
+        'Implemented API endpoints for a .NET Core web app to receive users’ insurance data and trigger financial calculations.',
+        'Developed adaptive React components featuring state persistence with Redux, to hook into back-end calculation services.',
+      ],
       link: 'https://www.manulife.com/',
       tech: [TECH.JAVASCRIPT, TECH.REACT, TECH.CSHARP, TECH.DOTNET],
       position: 'Full Stack Engineer Co-Op',
@@ -101,7 +117,10 @@ export const ExperienceSections: Record<EXPERIENCE_SECTION, ExperienceSection> =
     [EXPERIENCE_SECTION.TELEIOS]: {
       image: '/assets/teleios_logo.png',
       title: 'Teleios Systems',
-      descriptions: [''],
+      descriptions: [
+        'Developed a Bank Teller bot using Microsoft Bot Framework that featured handoff to humans and Microsoft’s NLU service.',
+        'Automated VM creation from snapshots to recover from possible downtime.',
+      ],
       link: 'https://www.teleios-systems.com/',
       tech: [TECH.CSHARP, TECH.DOTNET, TECH.AZURE],
       position: 'Software Developer Intern',
--- src/components/ProjectSection/sections.ts
+++ src/components/ProjectSection/sections.ts
@@ -6,31 +6,43 @@ export const ProjectSections: Record<PROJECT_SECTION, ProjectSection> = {
   [PROJECT_SECTION.LIFE_LOGS]: {
     image: '/assets/life_logs_logo.png',
     title: 'Life Logs',
-    descriptions: [''],
+    descriptions: [
+      'Developed an Android app for CS446 that uses NLP to generate meaningful summaries about your life after you tell it about your day.',
+      'Created a NodeJS server to handle authentication and data backups.',
+      'Designed a simple but impactful user experience on Figma.',
+    ],
     gitHubLink: 'https://github.com/cs446-software-design-architecture/app',
     figmaLink:
       'https://www.figma.com/file/FgGQL83PXAF6aDlG6DGxwi/V1?node-id=10%3A114',
-    tech: [TECH.JAVA, TECH.ANDROID, TECH.MONGO],
+    tech: [TECH.JAVA, TECH.ANDROID, TECH.NODE, TECH.MONGO],
   },
   [PROJECT_SECTION.SONG_STATS]: {
     image: '/assets/song_stats_logo.png',
     title: 'Song Stats',
-    descriptions: [''],
+    descriptions: [
+      'Built a web app for CS348 that uses Spotify APIs to show insightful summaries about songs and artists.',
+      'Implemented queries using TypeORM and MySql that fetche missing fragments of data from Spotify when necessary.',
+    ],
     gitHubLink: 'https://github.com/Dimatsa/cs348-database',
-    tech: [TECH.TYPESCRIPT, TECH.REACT, TECH.NODE, TECH.MYSQL],
+    tech: [TECH.TYPESCRIPT, TECH.REACT, TECH.NODE, TECH.MYSQL, TECH.DOCKER],
   },
   [PROJECT_SECTION.FINDER]: {
     image: '/assets/finder_logo.svg',
     title: 'Finder',
-    descriptions: [''],
+    descriptions: [
+      'Designed a mobile app for the Women Initiated Progress (WIP) Hackathon 2021 that allows users to monitor the location of their close friends and family members.',
+    ],
     figmaLink:
       'https://www.figma.com/file/pthaSVKKuRli2TGAi3l2JR/MVP?node-id=6%3A159',
-    tech: [TECH.FIGMA],
+    tech: [],
   },
   [PROJECT_SECTION.ANOTHER_NOTE_APP]: {
     image: '/assets/another_note_app_logo.png',
     title: 'Another Note App',
-    descriptions: [''],
+    descriptions: [
+      'Created a note app using React Concurrent Mode and Relay to experiment with the new frontend paradigm.',
+      'Developed a NodeJS server that used GraphQL subscriptions to update the client when notes were edited.',
+    ],
     gitHubLink: 'https://github.com/chriskrogh/another-note-app',
     tech: [
       TECH.TYPESCRIPT,
@@ -40,34 +52,45 @@ export const ProjectSections: Record<PROJECT_SECTION, ProjectSection> = {
       TECH.GRAPHQL,
       TECH.NODE,
       TECH.MONGO,
+      TECH.AWS,
     ],
   },
   [PROJECT_SECTION.ACS]: {
     image: '/assets/acs_logo.png',
     title: 'ACS',
-    descriptions: [''],
+    descriptions: [
+      'Collaborated on the website for the Association of Caribbean Students (ACS) at the University of Waterloo.',
+      'Added a voting feature that included video compression and uploads to AWS.',
+    ],
     link: 'https://uwacs.club/',
     gitHubLink: 'https://github.com/uwaterlooacs/acs',
-    tech: [TECH.TYPESCRIPT, TECH.REACT, TECH.NODE, TECH.MONGO],
+    tech: [TECH.TYPESCRIPT, TECH.REACT, TECH.NODE, TECH.MONGO, TECH.AWS],
   },
   [PROJECT_SECTION.KIT]: {
     image: '/assets/kit.png',
     title: 'KIT',
-    descriptions: [''],
+    descriptions: [
+      'Wrote a cron job that automates WhatsApp messages to friends to keep in touch duting the pandemic.',
+    ],
     gitHubLink: 'https://github.com/chriskrogh/kit',
     tech: [TECH.PYTHON],
   },
   [PROJECT_SECTION.VINGS]: {
     image: '/assets/vings.png',
     title: 'Vings',
-    descriptions: [''],
+    descriptions: [
+      'Developed and launched an expense tracker app that accumulated over 700 users on both iOS and Android.',
+      'Engineered a service using Azure functions that cached exchange rates, to reduce the number of calls to an external API.',
+    ],
     gitHubLink: 'https://github.com/chriskrogh/Vings',
     tech: [TECH.SWIFT, TECH.REACTNATIVE, TECH.AZUREFUNCTIONS],
   },
   [PROJECT_SECTION.SOCKET_CHAT]: {
     image: '/assets/socket_chat.png',
     title: 'Socket Chat',
-    descriptions: [''],
+    descriptions: [
+      'Created a chat web app using NodeJS, Express, and Socket.io to learn more about web sockets.',
+    ],
     gitHubLink: 'https://github.com/chriskrogh/socket-chat',
     tech: [TECH.JAVASCRIPT, TECH.HTML, TECH.CSS],
   },
