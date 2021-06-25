f503b0ade0fced5e2cc1af8f3b8dc20c495b4e84
Christopher Krogh <chris.krogh@outlook.com>
2021-06-25 09:57:47 -0400

    allow centering text

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
-const Typography = styled.span<{ underline?: boolean }>`
+const Typography = styled.span<{ underline?: boolean; center?: boolean }>`
   color: white;
   margin: 0;
   border-bottom: ${({ underline }) => (underline ? '1px solid white' : '')};
+  text-align: ${({ center }) => (center ? 'center' : '')};
 `;
 
 export default Typography;
