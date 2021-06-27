6cde43cebff03b5be190e6830ed883c378e2afcb
Christopher Krogh <chris.krogh@outlook.com>
2021-06-26 23:31:48 -0400

    fixed footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,18 +5,26 @@ import { TechMap } from 'components/TechStack/TechMap';
 import { TECH } from 'components/TechStack/types';
 import Typography from 'components/Typography';
 import React from 'react';
+import styled from 'styled-components';
+
+const RAIL_SPACING = 16;
+
+const Container = styled(Column)`
+  padding: 0 16px;
+  width: calc(100% - ${2 * RAIL_SPACING});
+`;
 
 const Footer: React.FC = () => {
   return (
-    <Column justifyContent="center" alignItems="center" fullWidth>
+    <Container justifyContent="center" alignItems="center">
       <Spacer height={16} />
-      <Typography as="p" secondary>
+      <Typography as="p" center secondary>
         This site was built using NextJS and is hosted on Vercel.
       </Typography>
       <Spacer height={8} />
       <IconLink src={TechMap[TECH.NEXT].icon} href={TechMap[TECH.NEXT].link} />
       <Spacer height={16} />
-    </Column>
+    </Container>
   );
 };
 
