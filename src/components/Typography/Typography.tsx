80eab6fa1659d107732b55728467232bcd1a2c6e
Christopher Krogh <chris.krogh@outlook.com>
2021-06-26 21:56:12 -0400

    add typography secondary color

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 
-const Typography = styled.span<{ underline?: boolean; center?: boolean }>`
-  color: white;
+type Props = {
+  underline?: boolean;
+  center?: boolean;
+  secondary?: boolean;
+};
+
+const Typography = styled.span<Props>`
+  color: ${({ secondary }) =>
+    secondary ? 'rgba(255, 255, 255, 0.5)' : 'white'};
   margin: 0;
   border-bottom: ${({ underline }) => (underline ? '1px solid white' : '')};
   text-align: ${({ center }) => (center ? 'center' : '')};
