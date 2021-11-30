66c65e629f15a783d5dc0a3d8654c2f0f31188a6
Christopher Krogh <chris.krogh@outlook.com>
2021-11-29 23:36:34 -0400

    removed context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import './index.css';
 
-import CategoryContextProvider from 'contexts/Category';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import GlobalStyle from 'theme/global/GlobalStyle';
@@ -8,9 +7,7 @@ import GlobalStyle from 'theme/global/GlobalStyle';
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
     <GlobalStyle />
-    <CategoryContextProvider>
-      <Component {...pageProps} />
-    </CategoryContextProvider>
+    <Component {...pageProps} />
   </>
 );
 
--- src/contexts/Category/types.ts
+++
@@ -1,4 +0,0 @@
-export enum CATEGORY {
-  EXPERIENCE = 'EXPERIENCE',
-  PROJECTS = 'PROJECTS',
-}
