aed43f56fb6c305828246b62e6408e0507d60736
Christopher Krogh <chris.krogh@outlook.com>
2021-11-29 23:48:11 -0400

    udpated theme folder structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import './index.css';
 
 import type { AppProps } from 'next/app';
 import React from 'react';
-import GlobalStyle from 'theme/global/GlobalStyle';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
